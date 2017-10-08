2d5d8e0def342fba175a9b86618672d06aa91191
AZNanoi <ezinanoi@gmail.com>
2017-10-08 15:26:15 +0200

    Added slider functionality to the image slider

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -9,25 +9,52 @@ export class ImageSliderComponent implements OnInit {
   @ViewChild('slides_container') slides_container: ElementRef;
   @ViewChild('slider_progress_bar') slider_progress_bar: ElementRef;
   public slides: any;
-  public displayed_slideIndex: number;
+  public item_onfocus_index: number;
   public bar_items: any;
 
   constructor() {
-    this.displayed_slideIndex = 3;
+    this.item_onfocus_index = 1;
   }
 
+  switchSlide(index): void {
+    const slides_wrapper = this.slides_container.nativeElement.getElementsByClassName('slides-wrapper')[0];
+    let margin_left = '';
+    if (index > this.item_onfocus_index) {
+      console.log('move left');
+      if (slides_wrapper.style.marginLeft) {
+        margin_left = (parseFloat(slides_wrapper.style.marginLeft) - 29.6) + '%';
+        console.log(margin_left);
+      }else {
+        margin_left = '-61.25%';
+        console.log(margin_left);
+      }
+      slides_wrapper.style.marginLeft = margin_left;
+    }else if (index < this.item_onfocus_index) {
+      console.log('move right');
+      if (slides_wrapper.style.marginLeft) {
+        margin_left = (parseFloat(slides_wrapper.style.marginLeft) + 29.6) + '%';
+        console.log(margin_left);
+      }else {
+        margin_left = '-61.25%';
+        console.log(margin_left);
+      }
+      slides_wrapper.style.marginLeft = margin_left;
+    }
+    this.item_onfocus_index = index;
+  }
+/*
   update_progress_bar(): void {
     for (let i = 0; i < this.slides.length; i++) {
       this.bar_items[i].style.backgroundColor = 'lightgray';
     }
-    this.bar_items[this.displayed_slideIndex - 1].style.backgroundColor = 'gray';
-  }
+    this.bar_items[this.item_onfocus_index - 1].style.backgroundColor = 'gray';
+  }*/
 
   ngOnInit() {
     this.bar_items = this.slider_progress_bar.nativeElement.getElementsByClassName('bar-item');
     this.slides = this.slides_container.nativeElement.getElementsByClassName('slide-wrapper');
 
-    this.update_progress_bar();
+    // this.update_progress_bar();
   }
 
 }
