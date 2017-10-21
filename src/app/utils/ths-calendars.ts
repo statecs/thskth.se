32dd6a17a66dcb9a184fa561e933d926ffa7c344
AZNanoi <ezinanoi@gmail.com>
2017-10-21 15:32:16 +0200

    Event calendar card is now in Swedish and English as well.

--- src/app/utils/ths-calendars.ts
+++ src/app/utils/ths-calendars.ts
@@ -1,34 +1,58 @@
 export const ths_calendars: any = [
     {
-        title: 'Events',
+        title:  {
+            en: 'Events',
+            sv: 'Evenemang'
+        },
         calendarId: 'ths.kth.se_uj2mnoprtjqmfkgth7u55tcjvo@group.calendar.google.com'
     },
     {
-        title: 'Assoications',
+        title:  {
+            en: 'Assoications',
+            sv: 'Associationer',
+        },
         calendarId: 'ths.kth.se_culqlm2b3gpngh3hjckupqqckk@group.calendar.google.com'
     },
     {
-        title: 'Central',
+        title:  {
+            en: 'Central',
+            sv: 'Central',
+        },
         calendarId: 'ths.kth.se_fndkaigtae85f2cg5jstef4mpk@group.calendar.google.com'
     },
     {
-        title: 'Chapters',
+        title:  {
+            en: 'Chapters',
+            sv: 'Sektioner',
+        },
         calendarId: 'ths.kth.se_u106rctpt330peusdd39aoctvg@group.calendar.google.com'
     },
     {
-        title: 'Education',
+        title:  {
+            en: 'Education',
+            sv: 'Utbildning',
+        },
         calendarId: 'ths.kth.se_i2s1ls8h5j2id5k342gsmu8m6c@group.calendar.google.com'
     },
     {
-        title: 'Future',
+        title:  {
+            en: 'Future',
+            sv: 'Framtida',
+        },
         calendarId: 'ths.kth.se_7h8vg7mk8dug011pp3hgqiup8o@group.calendar.google.com'
     },
     {
-        title: 'International',
+        title:  {
+            en: 'International',
+            sv: 'Internationell',
+        },
         calendarId: 'ths.kth.se_44cr4o5gflt0th51o45sum9vus@group.calendar.google.com'
     },
     {
-        title: 'Reception',
+        title:  {
+            en: 'Reception',
+            sv: 'Mottagning',
+        },
         calendarId: 'ths.kth.se_8mu5jbojebdnfta9amovri53dg@group.calendar.google.com'
     }/*,
     {
