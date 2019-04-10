69bf12f2984a88af78caad42c979ebe086dba7c6
Christopher State <statec@icloud.com>
2019-04-11 01:01:44 +0200

    Added Categories for news

--- src/app/interfaces-and-classes/post.ts
+++ src/app/interfaces-and-classes/post.ts
@@ -1,76 +1,107 @@
 export interface Author {
-    name: string;
-    email: string;
-    avatar_url: any;
+  name: string;
+  email: string;
+  avatar_url: any;
 }
-
+export interface Category {
+  name: string;
+  slug: string;
+}
 export interface IPost {
-    id: number;
-    title: string;
-    slug: string;
-    content: string;
-    image: any;
-    published_date: string;
-    last_modified: string;
-    author: Author;
+  id: number;
+  title: string;
+  slug: string;
+  content: string;
+  image: any;
+  published_date: string;
+  last_modified: string;
+  author: Author;
+  categories: Category[];
 }
 
-export class Post implements IPost{
-    id: number;
-    title: string;
-    slug: string;
-    content: string;
-    image: any;
-    published_date: string;
-    last_modified: string;
-    author: Author;
+export class Post implements IPost {
+  id: number;
+  title: string;
+  slug: string;
+  content: string;
+  image: any;
+  published_date: string;
+  last_modified: string;
+  author: Author;
+  categories: Category[];
 
-    static convertToPostType(data: any) {
-        const posts: Post[] = [];
-        data.forEach(p => {
-            const image: any = {};
-            if (p['_embedded']['wp:featuredmedia']) {
-                if (p['_embedded']['wp:featuredmedia'][0]) {
-                    if (p['_embedded']['wp:featuredmedia'][0].media_details.sizes) {
-                        if (p['_embedded']['wp:featuredmedia'][0].media_details.sizes.thumbnail) {
-                            image.thumbnail = p['_embedded']['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url;
-                        }
-                        if (p['_embedded']['wp:featuredmedia'][0].media_details.sizes.medium) {
-                            image.medium = p['_embedded']['wp:featuredmedia'][0].media_details.sizes.medium.source_url;
-                        }
-                        if (p['_embedded']['wp:featuredmedia'][0].media_details.sizes.large) {
-                            image.large = p['_embedded']['wp:featuredmedia'][0].media_details.sizes.large.source_url;
-                        }
-                    }
-                }
+  static convertToPostType(data: any) {
+    const posts: Post[] = [];
+    data.forEach(p => {
+      const image: any = {};
+      if (p["_embedded"]["wp:featuredmedia"]) {
+        if (p["_embedded"]["wp:featuredmedia"][0]) {
+          if (p["_embedded"]["wp:featuredmedia"][0].media_details.sizes) {
+            if (
+              p["_embedded"]["wp:featuredmedia"][0].media_details.sizes
+                .thumbnail
+            ) {
+              image.thumbnail =
+                p["_embedded"][
+                  "wp:featuredmedia"
+                ][0].media_details.sizes.thumbnail.source_url;
             }
-            posts.push({
-                id: p.id,
-                title: p.title.rendered,
-                slug: p.slug,
-                content: p.content.rendered,
-                image: image,
-                published_date: p.date,
-                last_modified: p.modified,
-                author: this.castDataToAuthorType(p),
-            });
-        });
-        return posts;
-    }
-
-    static castDataToAuthorType(data: any): Author {
-        const author: Author = {
-            name: data['_embedded'].author[0].name,
-            email: data.author.user_email,
-            avatar_url: {
-                thumbnail: data['_embedded'].author[0].avatar_urls['24'],
-                medium: data['_embedded'].author[0].avatar_urls['48'],
-                large: data['_embedded'].author[0].avatar_urls['96'],
+            if (
+              p["_embedded"]["wp:featuredmedia"][0].media_details.sizes.medium
+            ) {
+              image.medium =
+                p["_embedded"][
+                  "wp:featuredmedia"
+                ][0].media_details.sizes.medium.source_url;
             }
-        };
-        if (data.length > 0) {
-            author.name = data[0].name;
+            if (
+              p["_embedded"]["wp:featuredmedia"][0].media_details.sizes.large
+            ) {
+              image.large =
+                p["_embedded"][
+                  "wp:featuredmedia"
+                ][0].media_details.sizes.large.source_url;
+            }
+          }
         }
-        return author;
+      }
+
+      const categories: Category[] = [];
+      p.pure_taxonomies.categories.forEach(c => {
+        categories.push({
+          name: c.name,
+          slug: c.slug
+        });
+      });
+
+      posts.push({
+        id: p.id,
+        title: p.title.rendered,
+        slug: p.slug,
+        content: p.content.rendered,
+        image: image,
+        published_date: p.date,
+        last_modified: p.modified,
+        categories: categories,
+        author: this.castDataToAuthorType(p)
+      });
+    });
+    return posts;
+  }
+
+  static castDataToAuthorType(data: any): Author {
+    const author: Author = {
+      name: data["_embedded"].author[0].name,
+      email: data.author.user_email,
+      avatar_url: {
+        thumbnail: data["_embedded"].author[0].avatar_urls["24"],
+        medium: data["_embedded"].author[0].avatar_urls["48"],
+        large: data["_embedded"].author[0].avatar_urls["96"]
+      }
+    };
+    if (data.length > 0) {
+      author.name = data[0].name;
     }
+    return author;
+  }
 }
